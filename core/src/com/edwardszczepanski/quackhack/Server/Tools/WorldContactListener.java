8293dcf572c5350133a77123f4161c6f8e2424c5
edwardszczepanski <eszczepa@uoregon.edu>
2016-01-17 04:05:45 -0800

    Included sprite files and more.

--- core/src/com/edwardszczepanski/quackhack/Server/Tools/WorldContactListener.java
+++ core/src/com/edwardszczepanski/quackhack/Server/Tools/WorldContactListener.java
@@ -1,5 +1,7 @@
 package com.edwardszczepanski.quackhack.Server.Tools;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
@@ -12,7 +14,7 @@ import com.edwardszczepanski.quackhack.Server.Sprites.Player;
  * Created by edwardszc on 1/15/16.
  */
 public class WorldContactListener implements ContactListener {
-
+    private Music collision = Gdx.audio.newMusic(Gdx.files.internal("bounce.wav"));
 
 	@Override
 	public void beginContact(Contact contact) {
@@ -22,6 +24,7 @@ public class WorldContactListener implements ContactListener {
 		if (fixA.getUserData() instanceof Player || fixB.getUserData() instanceof Player) {
 			Fixture objA = fixA.getUserData() instanceof Player ? fixA : fixB;
             ((Player) objA.getUserData()).setTouching(true);
+            collision.play();
 		}
 	}
 
